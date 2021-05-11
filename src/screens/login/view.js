0636d9610b0ac7db779de8983f0b6af7ae254e75
semsamarasinghe@gmail.com <ErandaMadusanka>
2021-05-11 10:16:52 +0530

    sign in change

--- src/screens/login/view.js
+++ src/screens/login/view.js
@@ -1,18 +1,18 @@
 import Navbar from "../../common/navbar";
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
+// import Button from "@material-ui/core/Button";
+// import TextField from "@material-ui/core/TextField";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import Link from "@material-ui/core/Link";
+// import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import { Form, Button } from 'react-bootstrap';
 import "./style.css"
 
 
@@ -26,10 +26,10 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: "#28B463",
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%", 
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -44,7 +44,6 @@ export default function SignIn() {
     <div>
       <Navbar />
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
@@ -52,8 +51,8 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
+          <form className={classes.form}>
+            {/* <TextField
               variant="outlined"
               margin="normal"
               required
@@ -99,7 +98,28 @@ export default function SignIn() {
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
-            </Grid>
+            </Grid> */}
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Keep Me Logged in" />
+            </Form.Group>
+            <Button variant="primary" size="sm" className="loginbtn">
+              Log in
+            </Button>
+              <hr className="seperator" />
+            <Button variant="primary" size="sm" className="login-fb">
+              Log in with Facebook
+            </Button>
+            <div className="no-accont"><p>Don't have an account ? Get your <a href="/register">Free account Now</a></p></div>
+            {/* <div className="no-accont"><a href="/">Forgotten password?</a></div> */}
           </form>
         </div>
         <Box mt={8}>
